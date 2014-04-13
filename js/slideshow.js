2cd1bd1fbe9f7fd5d54835cdec2684949659f3c5
Jem Mawson <jem.mawson@gmail.com>
2014-04-13 16:53:32 +1000

    init

---
+++ js/slideshow.js
@@ -0,0 +1,24 @@
+$(function(){
+  $("#slides").slidesjs({
+    height: 600,
+    width: 443,
+    navigation: {
+      active: true
+    },
+    pagination: {
+      active: true
+    },
+    play: {
+      active: true,
+      effect: "fade",
+      interval: 6000,
+      auto: true,
+      swap: true,
+      pauseOnHover: false
+    },
+    fade: {
+      speed: 3000,
+      crossfade: true
+    }
+  });
+});
